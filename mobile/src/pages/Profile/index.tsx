ce84f9693fd42d7b6a9c4deba30a9efa36df04dd
Lucas Vasconcelos <lucas.vasconcelos@ax4b.com>
2020-10-14 19:36:47 -0300

    Página profile

--- mobile/src/pages/Profile/index.tsx
+++ mobile/src/pages/Profile/index.tsx
@@ -5,6 +5,9 @@ import Icon from 'react-native-vector-icons/AntDesign'
 import { StyleSheet, View, Text, SafeAreaView, Image, ScrollView } from "react-native";
 import Header from '../../components/Header';
 import api from '../../api';
+import { Form, List } from '../History/styles';
+import ProfileInfo from '../../components/UserInfo';
+import HistoricoList from '../../components/HistoricoList';
 
 interface dados {
   Id_Usuario: number;
@@ -12,123 +15,53 @@ interface dados {
   cpf_Usuario: string;
 };
 
+// interface dados{
+//   Id_Consulta: number;
+//   diagnostico_Consulta:string;
+//   dtAlteracao_Consulta:string;
+//   sintomasPaciente_Consulta:string;
+//   Id_Usuario:number;
+// };
+
 const Perfil: React.FC = () => {
 
-
-  const [user, setUser] = useState<dados>({} as dados);
+  const [users, setUsers] = useState<dados>({} as dados);
 
   const navigation = useNavigation();
-  useEffect(() => {
-    api.get('user').then((response) => {
-      setUser(response.data[0]);
+  useEffect(()=>{
+    api.get('users').then((response) =>{
+    setUsers(response.data);
+    console.log(response.data);
     })
-  })
-  console.log(user);
+  }, [])
+
+  function handleItem(data){
+    api.get('consultas/'+ data.Id_Usuario).then((response) =>{
+    navigation.navigate('Detalhes');
+    })
+
+  }
+
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Header />
-      <ScrollView showsVerticalScrollIndicator={false}>
-        {/* <View style={styles.titleBar}>
-          <Icon name="arrowleft" size={40} color="#52575D"></Icon>
-        </View> */}
-        <View style={{ alignSelf: "center" }}>
-          <View style={styles.profileImage}>
-            <Image source={require("./assets/profile.jpg")} style={styles.image} resizeMode="center"></Image>
-          </View>
-        </View>
-        <View style={{ alignSelf: "center" }}>
-          <Text style={styles.text}> Dados Pessoais</Text>
-          <View style={styles.info}>
-            <Text>nome:{user.nome_Usuario} </Text>
-            <Text>Cidade: Macapá</Text>
-            <Text>Cidade: Macapá2</Text>
-          </View>
-        </View>
-        <View style={styles.dm}>
-          <Icon name="edit" size={24} color="#FFF"></Icon>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
 
+    <>
+
+      <Container>
+        <Header/>
+         <List
+        keyboardShouldPeristTaps="handled"
+        data={users}
+        keyExtractor={item => item.Id_Usuario.toString()}
+        renderItem={({item})=> (<ProfileInfo data={item} /> )}
+        >
+        </List>
+
+     </Container>
+
+
+    </>
   );
 }
 
 export default Perfil;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#A0C9D9",
-    // alignItems: "center",
-    // justifyContent: "center"
-  },
-
-  text: {
-    fontFamily: "HelveticaNeue",
-    color: "#52575D"
-  },
-
-  image: {
-    flex: 1,
-    width: undefined,
-    height: undefined,
-  },
-
-  titleBar: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginTop: 24,
-    marginHorizontal: 16
-  },
-
-  profileImage: {
-    width: 150,
-    height: 150,
-    borderRadius: 100,
-    overflow: "hidden"
-
-  },
-
-  circle: {
-    width: 130,
-    height: 130,
-    borderRadius: 63,
-    borderWidth: 4,
-    borderColor: "white",
-    marginBottom: 10,
-
-  },
-
-  dm: {
-    backgroundColor: "#41444B",
-    position: "absolute",
-    top: 220,
-    left: 325,
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  text: {
-    fontFamily: "HelveticaNeue",
-    fontSize: 25,
-    color: "#000",
-    textAlign: "center",
-    marginBottom: 30,
-    marginTop: 20,
-  },
-
-  info: {
-    width: 300,
-    height: 420,
-    backgroundColor: "#E1E5E6",
-    padding: 10,
-    // marginTop: 30,
-    borderRadius: 20,
-    opacity: 10,
-  },
-
-});
