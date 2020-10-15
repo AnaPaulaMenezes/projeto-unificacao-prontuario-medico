ffa877bfdd2a1ea36eab701cd18872a4b50a1d59
Ana Paula Menezes <anapaularossi.577@gmail.com>
2020-10-15 11:03:32 -0300

    Correcao de erro com a pagina details no menu

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -1,24 +1,24 @@
-import React, {useContext} from 'react';
-import {createDrawerNavigator  } from '@react-navigation/drawer';
+import React, { useContext } from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import History from '../pages/History';
 import Profile from '../pages/Profile';
-import Detalhes from '../pages/Details';
+
 import Cadastro from '../pages/CadastroConsulta';
 
 const AppDrawer = createDrawerNavigator();
 
 
 const AppRoutes: React.FC = () => (
-<>
-  <AppDrawer.Navigator>
+  <>
+    <AppDrawer.Navigator>
 
-    <AppDrawer.Screen name="Histórico" component={History} />
-    <AppDrawer.Screen name="Usuario" component={Profile} />
-    <AppDrawer.Screen name="Detalhes" component={Detalhes} />
-    <AppDrawer.Screen name="Cadastro" component={Cadastro}/>
-  </AppDrawer.Navigator>
+      <AppDrawer.Screen name="Histórico" component={History} />
+      <AppDrawer.Screen name="Usuario" component={Profile} />
 
-</>
+      <AppDrawer.Screen name="Cadastro" component={Cadastro} />
+    </AppDrawer.Navigator>
+
+  </>
 );
 export default AppRoutes;
